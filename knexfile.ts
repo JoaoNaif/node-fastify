5a9dac0dccabfb76059fc62488a7036c648813a4
joaonaif <joaonaif@gmail.com>
2024-05-25 23:26:34 -0300

    initial commit

---
+++ knexfile.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import { config } from './src/database'
+
+export default config
+
+// Para knex reconhecer typescript precisa criar scripts knex package.json
